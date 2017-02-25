ceb83a8de19118de695d4000d4565f19ad63ee05
Frank <fnu.frangky@gmail.com>
2017-02-25 15:13:04 -0500

    ETC

--- app/src/main/java/org/intakers/intake/LoginActivity.java
+++ app/src/main/java/org/intakers/intake/LoginActivity.java
@@ -1,7 +1,11 @@
 package org.intakers.intake;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText username = (EditText) findViewById(R.id.UsernameEditText);
+        final EditText password = (EditText) findViewById(R.id.passwordEditText);
+        final Button sign_in_button = (Button) findViewById(R.id.sign_in_button);
+
+
+        sign_in_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (username.getText().length() == 0 || password.getText().length() == 0) {
+                    Toast.makeText(getApplicationContext(),"All Fields Required",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
