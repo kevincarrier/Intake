b86c7e0bbe3d2d66c2d948f134806e8cbd8091d4
George Keenan <georgekeenan@gmail.com>
2017-02-25 12:58:06 -0500

    First Commit, branch make check

--- app/src/main/java/org/intakers/intake/MainActivity.java
+++ app/src/main/java/org/intakers/intake/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Test to check branch
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
